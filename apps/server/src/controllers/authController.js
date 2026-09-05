c048d71ebf0060a79dc5196192dd1e6baa6b0e9f
ChicoGavi <sgaviria@uc.edu.co>
2026-09-04 20:56:03 -0500

    feat(auth): Create endpoint /api/auth/login

--- apps/server/src/controllers/authController.js
+++ apps/server/src/controllers/authController.js
@@ -1,5 +1,5 @@
 // IMPORT SERVICES
-import { registerService } from '../services/authService.js';
+import { registerService, loginService } from '../services/authService.js';
 
 import validateUser from '../utils/validateData.js';
 
@@ -16,6 +16,20 @@ export const register = async (req, res) => {
     await registerService(email, password, fullName);
 
     return res.status(201).json({ message: 'User registered with success!' });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    if (!req.body)
+      return res.status(400).json({ error: 'Complete the fields!' });
+
+    const { email, password } = req.body;
+    const token = await loginService(email, password);
+
+    return res.status(200).json({ token });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
