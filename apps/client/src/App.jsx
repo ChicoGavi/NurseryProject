0028e52bbaeee01a2b03e6c53ca33d70133fc8a7
ChicoGavi <sgaviria@uc.edu.co>
2026-09-07 14:49:13 -0500

    feat(frontend): Implement react-router-dom

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,10 +1,17 @@
 import RegisterForm from './components/Register.jsx';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// import {LoginForm} from './components/Login.jsx';
 
 function App() {
   return (
-    <>
-      <RegisterForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/*<Route path="/" element={<LoginForm/>}/>*/}
+        <Route path="/register" element={<RegisterForm />} />
+        {/* Redirección ante rutas no reconocidas (404) */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
