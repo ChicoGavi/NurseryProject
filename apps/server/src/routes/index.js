ae54e59236149f1a75935d62a65b9e9d4c987de1
ChicoGavi <sgaviria@uc.edu.co>
2026-09-15 15:58:22 -0500

    feat(backend): Create CRUD plants API

--- apps/server/src/routes/index.js
+++ apps/server/src/routes/index.js
@@ -2,10 +2,12 @@ import Router from 'express';
 import authRouter from './auth.js';
 import authToken from '../middlewares/authMiddleware.js';
 import categoryRouter from './category.js';
+import plantsRouter from './plants.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/category', authToken, categoryRouter);
+router.use('/plants', authToken, plantsRouter);
 
 export default router;
